04c8c15dd4b265eabfbbfa590e342b03c019d62d
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-09-14 19:14:23 +0600

    change something

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,13 @@ import app from './app';
 import config from './config';
 import { errorlogger, logger } from './shared/logger';
 
-
+// bootstrap function
 async function bootstrap() {
-
   const server: Server = app.listen(config.port, () => {
     logger.info(`Server running on port ${config.port}`);
   });
 
   const exitHandler = () => {
-
     if (server) {
       server.close(() => {
         logger.info('Server closed');
