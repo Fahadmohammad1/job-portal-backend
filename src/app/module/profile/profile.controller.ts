90c002ea8a5f024cc0cc4add683b5200c1e89b0a
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-09-16 22:31:45 +0600

    complete profile and socialPlatfrom api

--- src/app/module/profile/profile.controller.ts
+++ src/app/module/profile/profile.controller.ts
@@ -14,7 +14,54 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
+const myProfileFromDB = catchAsync(async (req: Request, res: Response) => {
+  const user = (req as any).user;
+  const result = await ProfileService.myProfileFromDB(user.userId);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Profile fetch successfully',
+    data: result,
+  });
+});
+const updateMyProfileIntoDB = catchAsync(
+  async (req: Request, res: Response) => {
+    const user = (req as any).user;
+    const result = await ProfileService.updateMyProfileIntoDB(
+      user.userId,
+      user.userId
+    );
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Profile update successfully',
+      data: result,
+    });
+  }
+);
+const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+  const result = await ProfileService.getByIdFromDB(req.params.id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Profile fetch successfully',
+    data: result,
+  });
+});
+const deleteByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+  const result = await ProfileService.deleteByIdFromDB(req.params.id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Profile delete successfully',
+    data: result,
+  });
+});
 
 export const ProfileController = {
   insertIntoDB,
+  myProfileFromDB,
+  updateMyProfileIntoDB,
+  deleteByIdFromDB,
+  getByIdFromDB,
 };
